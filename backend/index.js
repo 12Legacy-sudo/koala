4dcbfcea04db5937e0b8eab037ca9500d67a95af
12Legacy-sudo <nziokamtk@gmail.com>
2025-10-20 13:18:30 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,8 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "*",
+        origin: ['https://koalanotesapp.netlify.app','http://localhost5173'],
+        credentials: true
     })
 );
 
